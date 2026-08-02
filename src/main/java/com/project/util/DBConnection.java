43c1aa7ee7dd8569058a1d6094b798dade381eb9
Harsh006 <moteharsh156@gmail.com>
2026-08-02 19:56:13 +0530

    Update DBConnection.java

--- src/main/java/com/project/util/DBConnection.java
+++ src/main/java/com/project/util/DBConnection.java
@@ -1,18 +1,26 @@
 package com.project.util;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DBConnection {
 
 static String url , user , password ;
 	
-	public static Connection establishConnection() throws ClassNotFoundException, SQLException{
+	public static Connection establishConnection() throws ClassNotFoundException, SQLException, IOException{
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("config/db.properties");
+		prop.load(fis);
+
+		url = prop.getProperty("db.url");
+		user = prop.getProperty("db.user");
+		password = prop.getProperty("db.password");
 		
-		url = "jdbc:mysql://localhost:3306/blood_donor";
-		user = "root";
-		password = "Harsh@156";
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
 		Connection conn =  DriverManager.getConnection(url,user,password);
