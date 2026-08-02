ff072b354146d15059663478ced78320ffe2cf03
jemik <jemik@LAPTOP-1OOLF3HG>
2026-08-03 00:40:31 +0530

    Database Connection

---
+++ src/main/java/com/project/util/DBConnection.java
@@ -0,0 +1,32 @@
+package com.project.util;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class DBConnection {
+    private static String url;
+    private static String username;
+    private static String password;
+
+    static {
+        try {
+            Properties properties = new Properties();
+            FileInputStream file = new FileInputStream("db.properties");
+            properties.load(file);
+
+            url = properties.getProperty("db.url");
+            username = properties.getProperty("db.username");
+            password = properties.getProperty("db.password");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection establishConnection() throws Exception {
+        return DriverManager.getConnection(url, username, password);
+    }
+}
